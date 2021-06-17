bcd88b9b8f2bb5288670ba5eb8ea248c23841be3
rafdul <rafalduluk@gmail.com>
2021-06-17 14:25:34 +0200

    Add animation for banner

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+//typing
+const typingPlace = document.querySelector('.typing');
+const text = 'Look at my work';
+let letterIndex = 0;
+let oldTime = 0;
+const speed = 200; 
+console.log(typingPlace);
+
+const typing = (newTime) => {
+  const letter = text.substr(letterIndex, 1);
+  console.log('start typing');
+
+  if(newTime - oldTime > speed) {
+    if(letterIndex === text.length) {
+      console.log(letterIndex);
+      return stop;
+      // return setTimeout(() =>{
+      //   typingPlace.textContent = '';
+      //   letterIndex++;
+      //   requestAnimationFrame(typing);
+      // }, stop);
+    }
+    typingPlace.textContent += letter;
+    oldTime = newTime;
+    letterIndex++;
+  }
+
+  requestAnimationFrame(typing);
+};
+
+typing();

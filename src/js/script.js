5f6d9dc21dfd6dc82644825ebc42e59553f59c44
rafdul <rafalduluk@gmail.com>
2021-06-18 15:31:59 +0200

    Add icons in technology

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,41 @@
 'use strict';
 
 //typing
-const typingPlace = document.querySelector('.typing');
-const text = 'Look at my work';
-let letterIndex = 0;
+const box = document.querySelector('.banner');
+const text = [
+  'Frontend Web Developer', 'Look at my work'
+];
+let wordIndex = 0;
+let textIndex = 0;
 let oldTime = 0;
-const speed = 200; 
-console.log(typingPlace);
+const speed = 130; // czym większa wartość, tym wolniejszy typing
+const stop = 1500; // zatrzymanie między kolejnymi tekstami
+let activeDomElement = box;
 
 const typing = (newTime) => {
-  const letter = text.substr(letterIndex, 1);
-  console.log('start typing');
-
+  const letter = text[textIndex].substr(wordIndex,1);
+    
   if(newTime - oldTime > speed) {
-    if(letterIndex === text.length) {
-      console.log(letterIndex);
-      return stop;
-      // return setTimeout(() =>{
-      //   typingPlace.textContent = '';
-      //   letterIndex++;
-      //   requestAnimationFrame(typing);
-      // }, stop);
-    }
-    typingPlace.textContent += letter;
+    if(wordIndex === text[textIndex].length) {
+      if(textIndex === text.length-1) {
+        console.log('koniec animacji');
+        return;
+      }
+      return setTimeout(() =>{
+        textIndex++;
+        wordIndex = 0;
+        requestAnimationFrame(typing);
+      }, stop);
+    } else if(wordIndex === 0) {
+      const pMaker = document.createElement('p');
+      box.appendChild(pMaker);
+      activeDomElement = pMaker;
+    } 
+
+    activeDomElement.textContent += letter;
+
     oldTime = newTime;
-    letterIndex++;
+    wordIndex++;
   }
 
   requestAnimationFrame(typing);

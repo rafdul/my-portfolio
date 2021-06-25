622774e517e1d88327cddd184e2fcff3a33cb1ec
rafdul <rafalduluk@gmail.com>
2021-06-26 00:25:26 +0200

    Add animation for mobilemenu

--- src/js/script.js
+++ src/js/script.js
@@ -47,6 +47,7 @@ typing();
 const hamburger = document.querySelector('.menu__hamburger');
 const headerMobile = document.querySelector('.headerMobile');
 const navigationMobile = document.querySelector('.navigation-mobile');
+const hamburgerItem = Array.from(document.querySelectorAll('.menu__hamburger-item'));
 
 hamburger.addEventListener('click', () => {
   console.log('klik w hamburgera');
@@ -55,10 +56,17 @@ hamburger.addEventListener('click', () => {
     headerMobile.classList.remove('menuActive');
     navigationMobile.style.display = 'none';
     hamburger.classList.remove('changeHamburger');
+    for(let el of hamburgerItem) {
+      el.style.backgroundColor = '#000';
+      el.style.transition = '0.3s';
+    }
   } else {
     headerMobile.classList.add('menuActive');
     navigationMobile.style.display = 'block';
     hamburger.classList.add('changeHamburger');
+    for(let el of hamburgerItem) {
+      el.style.backgroundColor = '#f1f2f6';
+    }
   }
 });
 
@@ -66,4 +74,9 @@ navigationMobile.addEventListener('click', () => {
   console.log('klik w menu mobile');
   headerMobile.classList.remove('menuActive');
   navigationMobile.style.display = 'none';
+  hamburger.classList.remove('changeHamburger');
+  for(let el of hamburgerItem) {
+    el.style.backgroundColor = '#000';
+    el.style.transition = '0.3s';
+  }
 });

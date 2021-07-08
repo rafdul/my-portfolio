c8fc3e7cf12918e959f5cc4dcaabd99e3f156da2
rafdul <rafalduluk@gmail.com>
2021-07-08 21:52:02 +0200

    Add content in english

--- src/js/language.js
+++ src/js/language.js
@@ -4,7 +4,35 @@ import {eng, pol} from './languageVersion/languageVersion.js';
 const menuAboaut = document.querySelectorAll('.menuAbout');
 const menuProjects = document.querySelectorAll('.menuProjects');
 const menuContact = document.querySelectorAll('.menuContacts');
-console.log('menuAboaut', menuAboaut);
+const aboutTechnology = document.querySelector('.aboutTechnology');
+const aboutText = document.querySelector('#aboutText');
+const projectsPage = document.querySelectorAll('.projectsPage');
+const projectsCode = document.querySelectorAll('.projectsCode');
+const wideviewSubtitle = document.querySelector('#wideviewSubtitle');
+const wideviewDescr = document.querySelector('#wideviewDescr');
+const wideDashboardSubtitle = document.querySelector('#wideDashboardSubtitle');
+const wideDashboardDescr = document.querySelector('#wideDashboardDescr');
+const bulletinSubtitle = document.querySelector('#bulletinSubtitle');
+const bulletinDescr = document.querySelector('#bulletinDescr');
+const festivalSubtitle = document.querySelector('#festivalSubtitle');
+const festivalDescr = document.querySelector('#festivalDescr');
+const travelSubtitle = document.querySelector('#travelSubtitle');
+const travelDescr = document.querySelector('#travelDescr');
+const furnitureSubtitle = document.querySelector('#furnitureSubtitle');
+const furnitureDescr = document.querySelector('#furnitureDescr');
+const portfolioSubtitle = document.querySelector('#portfolioSubtitle');
+const portfolioDescr = document.querySelector('#portfolioDescr');
+const todoSubtitle = document.querySelector('#todoSubtitle');
+const todoDescr = document.querySelector('#todoDescr');
+const pizzeriaSubtitle = document.querySelector('#pizzeriaSubtitle');
+const pizzeriaDescr = document.querySelector('#pizzeriaDescr');
+const contactAddress = document.querySelector('.contactAddress');
+const contactName = document.querySelector('.contactName');
+const contactEmail = document.querySelector('.contactEmail');
+const contactTitle = document.querySelector('.contactTitle');
+const contactMessage = document.querySelector('.contactMessage');
+const contactButton = document.querySelector('.contactButton');
+
 
 // załadowanie tekstów przy defaultowym ustawieniu na angielski
 for(const el of menuAboaut) {
@@ -16,6 +44,40 @@ for(const el of menuProjects) {
 for(const el of menuContact) {
   el.innerText = eng.menu.contact;
 }
+aboutTechnology.innerText = eng.about.technology;
+aboutText.innerHTML = eng.about.text;
+for(const el of projectsPage) {
+  el.innerText = eng.projects.page;
+}
+for(const el of projectsCode) {
+  el.innerText = eng.projects.code;
+}
+wideviewSubtitle.innerText = eng.projects.wideview.subtitle;
+wideviewDescr.innerText = eng.projects.wideview.description;
+wideDashboardSubtitle.innerText = eng.projects.wideviewDashboard.subtitle;
+wideDashboardDescr.innerText = eng.projects.wideviewDashboard.description;
+bulletinSubtitle.innerText = eng.projects.bulletin.subtitle;
+bulletinDescr.innerText = eng.projects.bulletin.description;
+festivalSubtitle.innerText = eng.projects.festival.subtitle;
+festivalDescr.innerText = eng.projects.festival.description;
+travelSubtitle.innerText = eng.projects.travel.subtitle;
+travelDescr.innerText = eng.projects.travel.description;
+furnitureSubtitle.innerText = eng.projects.furniture.subtitle;
+furnitureDescr.innerText = eng.projects.furniture.description;
+portfolioSubtitle.innerText = eng.projects.portfolio.subtitle;
+portfolioDescr.innerText = eng.projects.portfolio.description;
+todoSubtitle.innerText = eng.projects.todo.subtitle;
+todoDescr.innerText = eng.projects.todo.description;
+pizzeriaSubtitle.innerText = eng.projects.pizzeria.subtitle;
+pizzeriaDescr.innerText = eng.projects.pizzeria.description;
+contactAddress.innerHTML = eng.contact.address;
+contactName.setAttribute('placeholder', eng.contact.name);
+contactName.setAttribute('data-error-text', eng.contact.nameError);
+contactEmail.setAttribute('data-error-text', eng.contact.emailError);
+contactTitle.setAttribute('placeholder', eng.contact.title);
+contactMessage.setAttribute('placeholder', eng.contact.message);
+contactMessage.setAttribute('data-error-text', eng.contact.messageError);
+contactButton.innerText = eng.contact.button;
 
 // zmiana wersji językowej w zależności od wartości na selekcie
 const select = document.querySelector('#language');
@@ -36,6 +98,40 @@ select.addEventListener('click', e => {
     for(const el of menuContact) {
       el.innerText = eng.menu.contact;
     }
+    aboutTechnology.innerText = eng.about.technology;
+    aboutText.innerHTML = eng.about.text;
+    for(const el of projectsPage) {
+      el.innerText = eng.projects.page;
+    }
+    for(const el of projectsCode) {
+      el.innerText = eng.projects.code;
+    }
+    wideviewSubtitle.innerText = eng.projects.wideview.subtitle;
+    wideviewDescr.innerText = eng.projects.wideview.description;
+    wideDashboardSubtitle.innerText = eng.projects.wideviewDashboard.subtitle;
+    wideDashboardDescr.innerText = eng.projects.wideviewDashboard.description;
+    bulletinSubtitle.innerText = eng.projects.bulletin.subtitle;
+    bulletinDescr.innerText = eng.projects.bulletin.description;
+    festivalSubtitle.innerText = eng.projects.festival.subtitle;
+    festivalDescr.innerText = eng.projects.festival.description;
+    travelSubtitle.innerText = eng.projects.travel.subtitle;
+    travelDescr.innerText = eng.projects.travel.description;
+    furnitureSubtitle.innerText = eng.projects.furniture.subtitle;
+    furnitureDescr.innerText = eng.projects.furniture.description;
+    portfolioSubtitle.innerText = eng.projects.portfolio.subtitle;
+    portfolioDescr.innerText = eng.projects.portfolio.description;
+    todoSubtitle.innerText = eng.projects.todo.subtitle;
+    todoDescr.innerText = eng.projects.todo.description;
+    pizzeriaSubtitle.innerText = eng.projects.pizzeria.subtitle;
+    pizzeriaDescr.innerText = eng.projects.pizzeria.description;
+    contactAddress.innerHTML = eng.contact.address;
+    contactName.setAttribute('placeholder', eng.contact.name);
+    contactName.setAttribute('data-error-text', eng.contact.nameError);
+    contactEmail.setAttribute('data-error-text', eng.contact.emailError);
+    contactTitle.setAttribute('placeholder', eng.contact.title);
+    contactMessage.setAttribute('placeholder', eng.contact.message);
+    contactMessage.setAttribute('data-error-text', eng.contact.messageError);
+    contactButton.innerText = eng.contact.button;
   }
   
   if(select.value === 'pol') {
@@ -48,5 +144,39 @@ select.addEventListener('click', e => {
     for(const el of menuContact) {
       el.innerText = pol.menu.contact;
     }
+    aboutTechnology.innerText = pol.about.technology;
+    aboutText.innerHTML = pol.about.text;
+    for(const el of projectsPage) {
+      el.innerText = pol.projects.page;
+    }
+    for(const el of projectsCode) {
+      el.innerText = pol.projects.code;
+    }
+    wideviewSubtitle.innerText = pol.projects.wideview.subtitle;
+    wideviewDescr.innerText = pol.projects.wideview.description;
+    wideDashboardSubtitle.innerText = pol.projects.wideviewDashboard.subtitle;
+    wideDashboardDescr.innerText = pol.projects.wideviewDashboard.description;
+    bulletinSubtitle.innerText = pol.projects.bulletin.subtitle;
+    bulletinDescr.innerText = pol.projects.bulletin.description;
+    festivalSubtitle.innerText = pol.projects.festival.subtitle;
+    festivalDescr.innerText = pol.projects.festival.description;
+    travelSubtitle.innerText = pol.projects.travel.subtitle;
+    travelDescr.innerText = pol.projects.travel.description;
+    furnitureSubtitle.innerText = pol.projects.furniture.subtitle;
+    furnitureDescr.innerText = pol.projects.furniture.description;
+    portfolioSubtitle.innerText = pol.projects.portfolio.subtitle;
+    portfolioDescr.innerText = pol.projects.portfolio.description;
+    todoSubtitle.innerText = pol.projects.todo.subtitle;
+    todoDescr.innerText = pol.projects.todo.description;
+    pizzeriaSubtitle.innerText = pol.projects.pizzeria.subtitle;
+    pizzeriaDescr.innerText = pol.projects.pizzeria.description;
+    contactAddress.innerHTML = pol.contact.address;
+    contactName.setAttribute('placeholder', pol.contact.name);
+    contactName.setAttribute('data-error-text', pol.contact.nameError);
+    contactEmail.setAttribute('data-error-text', pol.contact.emailError);
+    contactTitle.setAttribute('placeholder', pol.contact.title);
+    contactMessage.setAttribute('placeholder', pol.contact.message);
+    contactMessage.setAttribute('data-error-text', pol.contact.messageError);
+    contactButton.innerText = pol.contact.button;
   }
 });
--- src/js/languageVersion/languageVersion.js
+++ src/js/languageVersion/languageVersion.js
@@ -3,7 +3,60 @@ export const eng = {
     about: 'About me',
     projects: 'Projects',
     contact: 'Contact',
-  }
+  },
+  about: {
+    technology: 'Technologies',
+  },
+  projects: {
+    page: 'Page',
+    code: 'Code',
+    wideview: {
+      subtitle: 'Online Shop',
+      description: 'Final project during Kodilla bootcamp',
+    },
+    wideviewDashboard: {
+      subtitle: 'Admin Panel',
+      description: 'Own project, development of WideView-Shop',
+    },
+    bulletin: {
+      subtitle: 'Bulletin board with forms',
+      description: 'Project during Kodilla bootcamp',
+    },
+    festival: {
+      subtitle: 'Festival`s page with forms',
+      description: 'Project during Kodilla bootcamp',
+    },
+    travel: {
+      subtitle: 'Web page',
+      description: 'Project during Kodilla bootcamp',
+    },
+    furniture: {
+      subtitle: 'Online Shop',
+      description: 'Team project during Kodilla bootcamp',
+    },
+    portfolio: {
+      subtitle: 'Single Page App',
+      description: 'Own project with showcase page',
+    },
+    todo: {
+      subtitle: 'Single Page App',
+      description: 'Project during Kodilla bootcamp',
+    },
+    pizzeria: {
+      subtitle: 'Online Shop',
+      description: 'Project in Vanilla JS with cart',
+    },
+  },
+  contact: {
+    address: `<h4>Do you have a question? Call me ( <i class="fa fa-phone" aria-hidden="true"></i> 501 661 819), write to me ( <i class="fa fa-envelope" aria-hidden="true"></i> rafalduluk<i class="fa fa-at" aria-hidden="true"></i>gmail.com) or fill in the form!</h4>`,
+    name: 'Name',
+    nameError: 'Type your name (min. 3 characters)',
+    emailError: 'Type correct email',
+    title: 'Title',
+    message: 'Type your message',
+    messageError: 'Type message (max. 500 characters)',
+    button: 'Submit',
+  },
 };
 
 export const pol = {
@@ -11,5 +64,64 @@ export const pol = {
     about: 'O mnie',
     projects: 'Projekty',
     contact: 'Kontakt',
-  }
+  },
+  about: {
+    technology: 'Technologie',
+    text: 
+    `
+      <p>Nazywam się <strong>Rafał Duluk</strong> i jestem początkującym Front-End Developerem. Po kilkunastu latach pracy w marketingu i zarządzaniu produktami potrzebowałem nowych wyzwań. Chęć rozwoju i zapał do zdobywania nowych kwalifikacji stały się impulsem do zrewolucjonizowania ścieżki kariery. W ten sposób zdecydowałem o przekwalifikowaniu się, a przepustką do nowej branży – mam nadzieję – okaże się <strong>ukończenie intensywnego kursu</strong> Kodilli „Web Developer Plus” (Codemy S.A.).</p>
+      <p>Obecnie całą energię poświęcam na rozwijanie zdobytych umiejętności oraz <strong>poznawanie nowych technologii</strong>, które udoskonalą tworzone przeze mnie projekty.</p>
+      <p>Zachęcam do obejrzenia mojego portfolio, a <strong>jeśli jesteś zainteresowany kontaktem ze mną</strong>, skorzystaj z formularza lub umieszczonych poniżej danych.</p>
+    `
+  },
+  projects: {
+    page: 'Strona',
+    code: 'Kod',
+    wideview: {
+      subtitle: 'Sklep online',
+      description: 'Projekt końcowy w ramach kursu Kodilla',
+    },
+    wideviewDashboard: {
+      subtitle: 'Panel administracyjny',
+      description: 'Projekt własny, rozbudowa sklepu WideView',
+    },
+    bulletin: {
+      subtitle: 'Tablica ogłoszeń z formularzami',
+      description: 'Projekt w ramach kursu Kodilla',
+    },
+    festival: {
+      subtitle: 'Strona festiwalu z formularzami',
+      description: 'Projekt w ramach kursu Kodilla',
+    },
+    travel: {
+      subtitle: 'Strona internetowa',
+      description: 'Projekt w ramach kursu Kodilla',
+    },
+    furniture: {
+      subtitle: 'Sklep online',
+      description: 'Strona powstała w czasie kursu Kodilla jako praca zespołowa',
+    },
+    portfolio: {
+      subtitle: 'Single Page App',
+      description: 'Strona-wizytówka, projekt własny',
+    },
+    todo: {
+      subtitle: 'Single Page App',
+      description: 'Projekt w ramach kursu Kodilla',
+    },
+    pizzeria: {
+      subtitle: 'Strona sklepu z koszykiem',
+      description: 'Projekt napisany w Vanilla JS',
+    },
+  },
+  contact: {
+    address: `<h4>Masz pytanie? Zadzwoń ( <i class="fa fa-phone" aria-hidden="true"></i> 501 661 819), napisz ( <i class="fa fa-envelope" aria-hidden="true"></i> rafalduluk<i class="fa fa-at" aria-hidden="true"></i>gmail.com) lub wypełnij formularz!</h4>`,
+    name: 'Imię',
+    nameError: 'Podaj imię (min. 3 znaki)',
+    emailError: 'Wpisz poprawny email',
+    title: 'Tytuł',
+    message: 'Wpisz wiadomość',
+    messageError: 'Wpisz treść wiadomości (max. 500 znaków)',
+    button: 'Wyślij',
+  },
 };
